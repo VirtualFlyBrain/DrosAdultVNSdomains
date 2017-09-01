b23873477eb8c186b980d1636d17c726a6045e46
Jenkins CI Server <jenkins@bocian.inf.ed.ac.uk>
2017-09-01 16:18:31 +0100

    new bounded wlz domains

--- runOnBocian.sh
+++ runOnBocian.sh
@@ -1,7 +1,7 @@
 cd /partition/bocian/VFBTools/DrosAdultTAGdomains/
 git pull
 source ../python-modules-2.6/bin/activate
-python ../IndexStackConv/nrrd2wlzDom.py individualDomainFiles/domain template/background.wlz ../Woolz2013Full/bin/ '/disk/data/VFBTools/Fiji/ImageJ-linux64 --headless' 0.46 0.4612588
+python /disk/data/VFB/IMAGE_DATA/StackProcessing/scripts/nrrd2wlzDom.py individualDomainFiles/domain template/background.wlz ../Woolz2013Full/bin/ '/disk/data/VFBTools/Fiji/ImageJ-linux64 --headless' 0.46 0.4612588
 deactivate
 mv out.wlz combinedIndexFiles/composite.wlz
 
